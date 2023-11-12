44b3bc1643e37a0ed99c011290cd2f394566caea
Swapnil <rupakheteeswapnil@gmail.com>
2023-11-12 16:38:09 +0545

    working

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../pages/Todo.css'
+import TodoContent from './TodoContent'
 
 function Todo() {
   return (
     <div className='todo-container'>
+      <h1 className='title'>Your Todo's</h1>
       <div className='todos'>
+        <TodoContent/>
        
         
         
---
+++ src/pages/TodoContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TodoContent() {
+  return (
+    <div>TodoContent</div>
+  )
+}
+
+export default TodoContent

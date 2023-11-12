ab0c30803d254946e09c93d45d9b220c668a845c
Swapnil <rupakheteeswapnil@gmail.com>
2023-11-12 17:45:14 +0545

    workds

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
 import '../pages/Todo.css'
 import TodoContent from './TodoContent'
+import {MdDownloading} from 'react-icons/md';
+import {FaPercent} from 'react-icons/fa';
 
 function Todo() {
   return (
     <div className='todo-container'>
-      <h1 className='title'>Your Todo's</h1>
+      <div className='stats'>
+        <h1>Todo Stats</h1>
+        <div className='stats-container'>
+        <div className='progress-status'>
+          <h3>Status:Ongoing <MdDownloading/></h3>
+        </div>
+        <div className='progress-bar'>
+           <FaPercent/>90
+           
+        </div>
+        <div className='tasks'>
+        </div>
+      </div>
+      </div>
+      <h1 className='title'>Main Goal</h1>
       <div className='todos'>
+        <TodoContent/>
+        <TodoContent/>
+        <TodoContent/>
         <TodoContent/>
        
         
         
       </div>
+      <h1 className='second-title'>Small Task's</h1>
+
     </div>
   )
 }
--- src/pages/TodoContent.jsx
+++ src/pages/TodoContent.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../pages/Todo.css'
+import {AiFillDelete} from 'react-icons/ai';
+import {BsCheckCircle,BsCheckCircleFill} from"react-icons/bs";
+
 
 function TodoContent() {
+    const tasks="Jack Off"
+    const[checkbox,setCheckbox]=useState(false)
+    const togglecheck=()=>{
+        if (checkbox==true){
+            setCheckbox(false);
+
+        }
+        else if (checkbox==false)
+        {
+            setCheckbox(true);
+
+        }
+        
+    }
   return (
-    <div>TodoContent</div>
+    <div className='content-container'>
+        <div className='data'>
+            <div className='tick-text'>
+
+            { (checkbox) ?
+            <BsCheckCircle className='unticked' onClick={togglecheck} />
+            :
+            <BsCheckCircleFill className='ticked'onClick={togglecheck}/>}
+            <div className={(checkbox)?"ongoing":"completed"}>
+            {tasks}
+            </div>
+            </div>
+            
+            <AiFillDelete className='del'/>
+
+        </div>
+    </div>
   )
 }
 

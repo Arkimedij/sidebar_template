5c2763cbf0835e17633f28d9125dbb0c396a0c6d
Swapnil <rupakheteeswapnil@gmail.com>
2023-11-17 14:54:50 +0545

    damn

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,96 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../pages/Todo.css'
-import TodoContent from './TodoContent'
-import {MdDownloading} from 'react-icons/md';
-import {FaPercent} from 'react-icons/fa';
-import { useState,useEffect } from 'react';
+import { RiTodoFill } from "react-icons/ri";
+import { MdOutlineAddCircleOutline } from "react-icons/md";
+import { AiFillDelete } from "react-icons/ai";
+import { FaCheckCircle } from "react-icons/fa";
+
+
+
 
 function Todo() {
-  const[tasks,setTasks]=useState([]);
-  const[taskName,setTaskName]=useState('');
-
-  useEffect(()=>{
-    if(tasks === null ||tasks.length===0 )return;
-    localStorage.setItem('tasks',JSON.stringify(tasks));
-  },[tasks]);
-
-  useEffect(()=>{
-    const tasks=JSON.parse(localStorage.getItem('tasks'));
+  const[inputData,setInputData] = useState('');
+  const[items,setItems] = useState([]);
+  const addItem =()=>
+  {
     
-    if (tasks !== null) {
-      setTasks(tasks);
-    }
-    
-  },[])
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        addTask(taskName);
-        setTaskName('');
-    }
-    function addTask(taskName) {
-      if(taskName==="") return alert('please enter task name');
-      setTasks(prev=>{
-          return [...prev,{name:taskName,done:true}];
-        });
-    }
-    function removeTask(indexToRemove){
-      setTasks(prev=>{
-        return prev.filter((taskObject,index)=>{
-          
-          return index!==indexToRemove
-        });
-      });
-    }  
-    
+
+  }
   
-
   
   return (
-    <div className='todo-container'>
-      <div className='stats'>
-        <h1>Todo Stats</h1>
-        <div className='stats-container'>
-        <div className='progress-status'>
-          <h3>Status:Ongoing <MdDownloading/></h3>
-        </div>
-        <div className='progress-bar'>
-           <FaPercent/>90
-           
-        </div>
-        <div className='tasks'>
-        </div>
-      </div>
-      </div>
-      <h1 className='title'>Main Goal</h1>
-      <form onSubmit={handleSubmit}>
-        <button>+</button>
-        <input type="text" 
-        value={taskName} 
-        onChange={(e)=>setTaskName(e.target.value)} placeholder='your next task'>
+    <>
+      <div className='main-div'>
+        <div className='sub-main-div'>
+          
+          <div className='image-container'>
+            <RiTodoFill className='image'/>
+            <figcaption>ToDO's</figcaption>
+          </div>
+          
+          <div className='todo-container'>
+            <input type='text' placeholder='Add todos' className='search' value={inputData} onChange={(e)=>setInputData(e.target.value)} />
+            <div className='add-button'><MdOutlineAddCircleOutline className='add-button-todo' onClick={addItem}/></div>
+            
+          </div>
 
-        </input>
-      </form>
+          <div className='show-items'>
+            <div className='each-item'>
+              <FaCheckCircle />
+              
+              <h4>Class</h4>
+              <AiFillDelete />
+            </div>
+          </div>
+          
+          
+        </div>
+        
+        
+          
+        
+        
+      </div>
       
-      
-
-
-      <div className='todos'>
-        <TodoContent onAdd={addTask} name={setTaskName}/>
-        {tasks&&tasks.map((tasks,index)=>(
-        <TodoContent {...tasks}
-        
-        onTrash={()=>removeTask(index)}
-        />
-      ))}
-        
-       
-        
-        
-      </div>
-      <h1 className='second-title'>Small Task's</h1>
-
-    </div>
+    </>
   )
 }
 
--- src/pages/TodoContent.jsx
+++ src/pages/TodoContent.jsx
@@ -1,44 +1,14 @@
-import React, { useState } from 'react'
+
 import '../pages/Todo.css'
-import {AiFillDelete} from 'react-icons/ai';
-import {BsCheckCircle,BsCheckCircleFill} from"react-icons/bs";
+
 
 
 function TodoContent({name,onTrash}) {
-    const empty = "empty"
     
-    const[checkbox,setCheckbox]=useState(false)
-    const togglecheck=()=>{
-        if (checkbox===true){
-            setCheckbox(false);
-
-        }
-        else if (checkbox===false)
-        {
-            setCheckbox(true);
-
-        }
-        
-    }
     
   return (
     <div className='content-container'>
-        <div className='data'>
-            <div className='tick-text'>
-
-            { (checkbox) ?
-            <BsCheckCircle className='unticked' onClick={togglecheck} />
-            :
-            <BsCheckCircleFill className='ticked'onClick={togglecheck}/>}
-            <div className={(checkbox)?"ongoing":"completed"}>
-            
-            {name}
-            </div>
-            </div>
-            
-            <AiFillDelete className='del' onClick={onTrash} />
-
-        </div>
+        
     </div>
   )
 }

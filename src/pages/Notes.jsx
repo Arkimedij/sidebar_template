e5fd66a88108ae47f7e5e5f0a4d7cb4ad279f60a
Swapnil <rupakheteeswapnil@gmail.com>
2023-11-20 23:44:05 +0545

    fortoday

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,44 +1,95 @@
-import React, { useState } from 'react'
-import { FaRegNoteSticky } from "react-icons/fa6";
+import React, { useEffect, useState } from 'react';
+import { FaRegNoteSticky } from 'react-icons/fa6';
+import { v4 as uuid } from 'uuid';
 
 function Notes() {
-  const [inputText,setInputText] = useState("");
-  const [notes,setNotes]=useState([]);
+  const [inputText, setInputText] = useState('');
+  const [notes, setNotes] = useState([]);
+  const char = 260;
+  const charLimit = char - inputText.length;
 
-  const saveHandler=() => {
-
-
+  const saveHandler = (value) => {
+    setInputText(value);
+    setNotes((prevNotes) => [
+      ...prevNotes,
+      {
+        id: uuid(),
+        text: value,
+      },
+    ]);
+    setInputText('');
   };
+
+  const deleteHandler = (id) => {
+    setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
+  };
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem('Notes'));
+    if (data) {
+      setNotes(data);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem('Notes', JSON.stringify(notes));
+  }, [notes]);
+
   return (
-    <div className='note-container'>
-      <div className='logo-note'>
-        <FaRegNoteSticky className='note-logo'/>
-        <h4 className='sub-title-note'>Write down your 💡 ideas</h4>
-        </div>
-      
-      <div className='note-sub-container'>
-        <div className='note'>
+    <div className="note-container">
+      <div className="logo-note">
+        <FaRegNoteSticky className="note-logo" />
+        <h4 className="sub-title-note">Write down your 💡 ideas</h4>
+      </div>
+
+      <div className="note-sub-container">
+        <div className="note">
           <textarea
             cols={45}
             rows={7}
             maxLength={260}
-            onChange={(e)=>saveHandler(e.target.value)}
-            placeholder='Type...'
-            ></textarea>
-            <div className='note-footer'>
-              <span className='note-footer-label'>{} Characters left</span>
-              <div className='button-container'>
-              <button className='note-save' onClick={saveHandler}>Save</button>
-
-              </div>
-              
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            placeholder="Type..."
+          ></textarea>
+          <div className="note-footer">
+            <span className="note-footer-label">{charLimit} Characters left</span>
+            <div className="button-container">
+              <button className="note-save" onClick={() => saveHandler(inputText)}>
+                Save
+              </button>
             </div>
+          </div>
         </div>
 
+        {/* Display saved notes */}
+        
       </div>
+      <div className="saved-notes">
+          {notes.map((note) => (
+            
+              
+              
+              <div className="note-sub-container-data"key={note.id}>
+                    <div className="note">
+                      <div className='textarea'>{note.text}</div>
+                      
+                      <div className="note-footer">
+                        <span className="note-footer-label">{charLimit} Characters left</span>
+                        <div className="button-container">
+                        <button onClick={() => deleteHandler(note.id)}>Delete</button>
+                        </div>
+                      </div>
+                    </div>
 
+                    {/* Display saved notes */}
+                    
+                  </div>
+            
+          ))}
+        </div>
     </div>
-  )
+  );
 }
 
-export default Notes
+export default Notes;
